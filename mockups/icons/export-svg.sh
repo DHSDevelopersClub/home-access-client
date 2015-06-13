e537f18cc184d8b643ee91b14a6c90ec4fae2992
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2015-06-13 15:33:50 -0700

    Add script to automaticly generate .png icons

---
+++ mockups/icons/export-svg.sh
@@ -0,0 +1,7 @@
+
+rm ../../app/images/touch/*
+rsvg-convert icon.svg -w 152 -h 152 -o ../../app/images/touch/apple-touch-icon.png
+rsvg-convert icon.svg -w 192 -h 192 -o ../../app/images/touch/chrome-touch-icon-192x192.png
+rsvg-convert icon.svg -w 128 -h 128 -o ../../app/images/touch/icon-128x128.png
+rsvg-convert icon.svg -w 144 -h 144 -o ../../app/images/touch/ms-icon-144x144.png
+rsvg-convert icon.svg -w 144 -h 144 -o ../../app/images/touch/ms-touch-icon-144x144-precomposed.png
